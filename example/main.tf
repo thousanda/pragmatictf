219a2abc715debc68ae1d4189becbd3cc80bb873
thousanda <k0k1senda@gmail.com>
2022-02-16 17:11:00 +0900

    add security_group to access ec2

--- example/main.tf
+++ example/main.tf
@@ -2,22 +2,29 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  ami           = "ami-0f9ae750e8274075b"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+}
+
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
 }
