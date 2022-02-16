8c19d0a27777b75d4ca6ac9a491915d62e378765
thousanda <k0k1senda@gmail.com>
2022-02-16 18:20:58 +0900

    fix: add security_group to access ec2

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 resource "aws_security_group" "example_ec2" {
   name = "example-ec2"
 
@@ -11,7 +7,15 @@ resource "aws_security_group" "example_ec2" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
