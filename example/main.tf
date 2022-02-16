bcdd066d63566eddea6cd68c2f3b0907e239912f
thousanda <k0k1senda@gmail.com>
2022-02-16 16:50:34 +0900

    use data source

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,19 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0f9ae750e8274075b"
-  instance_type = "t3.micro"
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
 
-output "example_instance_id" {
-  value = aws_instance.example.id
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = "t3.micro"
 }
