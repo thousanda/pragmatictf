0da96eef0a9c0a46a373773b24c34c3b5ca62303
thousanda <k0k1senda@gmail.com>
2022-02-16 20:27:35 +0900

    use template for apache installation

--- example/main.tf
+++ example/main.tf
@@ -16,13 +16,20 @@ resource "aws_security_group" "example_ec2" {
   }
 }
 
+data "template_file" "httpd_user_data" {
+  template = file("./user_data.sh.tpl")
+
+  vars = {
+    package = "httpd"
+  }
+}
 
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
-  user_data = file("./user_data.sh")
+  user_data = data.template_file.httpd_user_data.rendered
 }
 
 output "example_public_dns" {
