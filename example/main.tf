cb83abeb9c697fe67cff484bf61ff38077819359
thousanda <k0k1senda@gmail.com>
2022-02-16 20:09:12 +0900

    external script to install apache

--- example/main.tf
+++ example/main.tf
@@ -16,18 +16,13 @@ resource "aws_security_group" "example_ec2" {
   }
 }
 
-variable "env" {}
 
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
-  instance_type = var.env == "prod" ? "m5.large" : "t3.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+  user_data = file("./user_data.sh")
 }
 
 output "example_public_dns" {
