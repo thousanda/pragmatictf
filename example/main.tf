08b1792690d6976a755adcd455e146c41aab89e2
thousanda <k0k1senda@gmail.com>
2022-02-16 16:30:34 +0900

    replace instance test

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "example"
-  }
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
