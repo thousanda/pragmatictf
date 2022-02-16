421371fa1c9e04e4141e676ea6e9bbcd2f1094ae
thousanda <k0k1senda@gmail.com>
2022-02-16 16:35:14 +0900

    use variable test

--- example/main.tf
+++ example/main.tf
@@ -1,10 +1,8 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
-  instance_type = "t3.micro"
-
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+  instance_type = var.example_instance_type
 }
