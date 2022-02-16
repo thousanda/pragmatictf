0c04f4f17649ed992b745132e7edbb2e6f3f1764
thousanda <k0k1senda@gmail.com>
2022-02-16 16:43:22 +0900

    output can output some values

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,8 @@
-locals {
-  example_instance_type = "t3.micro"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
-  instance_type = local.example_instance_type
+  instance_type = "t3.micro"
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
 }
